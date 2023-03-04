cd82b13916de208552fe13808bcf269b1972ccfa
jouniwho <55877078+jouniwho@users.noreply.github.com>
2023-03-04 20:41:32 +0200

    reformatting

--- fitnessbuddy/resources/measurement.py
+++ fitnessbuddy/resources/measurement.py
@@ -1,72 +1,93 @@
 import json
+from datetime import datetime
 from flask import Response, request
 from flask import url_for
 from flask_restful import Resource
 from fitnessbuddy.models import db, Measurements
 from jsonschema import validate, ValidationError
 from werkzeug.exceptions import UnsupportedMediaType, BadRequest
-from datetime import datetime
+from sqlalchemy.exc import IntegrityError
 
 class MeasurementsCollection(Resource):
+    """
+    Class for measurements
+    """
     def get(self, user):
-        #initialize response body
-        body = {
-            "measurements": []}
-        #find all users and add them to the response
+        # initialize response body
+        body = {"measurements": []}
+        # find all users and add them to the response
         for item in Measurements.query.filter_by(user=user).all():
             measurement_item = item.serialize()
             body["measurements"].append(measurement_item)
 
-        #return users
+        # return users
         return Response(json.dumps(body), 200, mimetype="application/json")
-    
+
     def post(self, user):
-        #check that request is json
+        # check that request is json
         if not request.json:
             raise UnsupportedMediaType
-        #check json schema 
+        # check json schema
         try:
             validate(request.json, Measurements.json_schema())
-        except ValidationError as e:
-            raise BadRequest(description=str(e))
-        
-        #initialize new user using deserializer
-        measurement = Measurements()
-        measurement.deserialize(request.json)
-        measurement.user=user
+        except ValidationError as error:
+            raise BadRequest() from error
 
-        #add new user to database
+        # initialize new user using deserializer
+        try:
+            measurement = Measurements()
+            measurement.deserialize(request.json)
+            measurement.user = user
+        except (KeyError, ValueError, IntegrityError) as error:
+            raise BadRequest() from error
+        # add new user to database
         db.session.add(measurement)
         try:
             db.session.commit()
-        except Exception as e:
-            return Response(str(e), status=400)
+        except (KeyError, ValueError, IntegrityError):
+            return Response(str(error), status=400)
 
-        return Response(status=201, headers={"location":str(url_for("api.measurementsitem", user=measurement.user, measurements=measurement))})
+        return Response(
+            status=201,
+            headers={
+                "location": str(
+                    url_for(
+                        "api.measurementsitem",
+                        user=measurement.user,
+                        measurements=measurement,
+                    )
+                )
+            },
+        )
+
 
 class MeasurementsItem(Resource):
     def get(self, user, measurements):
         if user != measurements.user:
-            raise BadRequest(description="requested measurement does not correspond to requested user")
+            raise BadRequest(
+                description="requested measurement does not correspond to requested user"
+            )
         body = measurements.serialize()
         return Response(json.dumps(body), 200, mimetype="application/json")
-        
+
     def put(self, user, measurements):
-        #check that request is json
+        # check that request is json
         if not request.json:
             raise UnsupportedMediaType
         if request.json["user_id"]:
             if not request.json["user_id"] == user.id:
-                raise BadRequest(description="UserID mismatch in request address and body")
+                raise BadRequest(
+                    description="UserID mismatch in request address and body"
+                )
         else:
             request.json["user_id"] = user.id
-        #check json schema 
+        # check json schema
         try:
             validate(request.json, Measurements.json_schema())
-        except ValidationError as e:
-            raise BadRequest(description=str(e))
-        
-        #update database entry
+        except Exception as error:
+            raise BadRequest() from error
+
+        # update database entry
         try:
             measurements.date = datetime.fromisoformat(request.json["date"])
             measurements.weight = request.json["weight"]
@@ -74,15 +95,22 @@ class MeasurementsItem(Resource):
             measurements.calories_out = request.json["calories_out"]
             measurements.user_id = request.json["user_id"]
             db.session.commit()
-        except Exception as e:
-            return Response(str(e), status=400)
-        return Response(status=204, headers={"location":str(url_for("api.measurementsitem", user=measurements.user, measurements=measurements))})
-    
-    def delete(self, user, measurements):
-        try:
-            db.session.delete(measurements)
-            db.session.commit()
-        except Exception as e:
-            Response(str(e), status=400)
+        except (KeyError, ValueError, IntegrityError) as error:
+            return Response(str(error), status=400)
+        return Response(
+            status=204,
+            headers={
+                "location": str(
+                    url_for(
+                        "api.measurementsitem",
+                        user=measurements.user,
+                        measurements=measurements,
+                    )
+                )
+            },
+        )
 
+    def delete(self, user, measurements):
+        db.session.delete(measurements)
+        db.session.commit()
         return Response(status=204)
--- test/test_measurements.py
+++ test/test_measurements.py
@@ -1,15 +1,13 @@
+import json
+import tempfile
+import os
+from datetime import datetime
+import pytest
 from fitnessbuddy.models import Exercise, User, Measurements
 from fitnessbuddy import create_app, db
 from sqlalchemy.engine import Engine
 from sqlalchemy import event
 import tools.populate_database
-from datetime import datetime
-import json
-import pathlib
-import tempfile
-import os
-import pytest
-
 
 @event.listens_for(Engine, "connect")
 def set_sqlite_pragma(dbapi_connection, connection_record):
@@ -17,41 +15,44 @@ def set_sqlite_pragma(dbapi_connection, connection_record):
     cursor.execute("PRAGMA foreign_keys=ON")
     cursor.close()
 
+
 # based on http://flask.pocoo.org/docs/1.0/testing/
 # we don't need a client for database testing, just the db handle
 @pytest.fixture
 def client():
     tempfile.tempdir = os.path.dirname(__file__)
     db_fd, db_fname = tempfile.mkstemp(prefix="temppytest_")
-    config = {
-        "SQLALCHEMY_DATABASE_URI": "sqlite:///" + db_fname,
-        "TESTING": True
-    }
-    
+    config = {"SQLALCHEMY_DATABASE_URI": "sqlite:///" + db_fname, "TESTING": True}
+
     app = create_app(config)
-    
+
     with app.app_context():
         db.create_all()
         tools.populate_database.populate_database(db, app)
-        
+
     yield app.test_client()
-    
+
     os.close(db_fd)
-    #os.unlink(db_fname)
+    # os.unlink(db_fname)
+
 
 def get_dummy_data_by_userid(id):
-    ddata_path = os.path.join(os.path.dirname(__file__), "..", "tools", "dummy_data.json")
-    with open(ddata_path) as f:
-        cont = json.load(f)
-    cont = cont["Measurements"] #Only interested in exercise records
+    ddata_path = os.path.join(
+        os.path.dirname(__file__), "..", "tools", "dummy_data.json"
+    )
+    with open(ddata_path) as file:
+        cont = json.load(file)
+    cont = cont["Measurements"]  # Only interested in exercise records
     res = []
     for item in cont:
         if item.get("user_id") == id:
-            #convert timestamp to format in reponse (iso format) and duration to float
-            item["date"] = datetime.isoformat(datetime.strptime(item['date'], '%d/%m/%y %H:%M:%S'))
+            # convert timestamp to format in reponse (iso format) and duration to float
+            item["date"] = datetime.isoformat(
+                datetime.strptime(item["date"], "%d/%m/%y %H:%M:%S")
+            )
             item["weight"] = float(item["weight"])
             item["calories_in"] = float(item["calories_in"])
-            item["calories_out"]= float(item["calories_out"])
+            item["calories_out"] = float(item["calories_out"])
             res.append(item)
     return res
 
@@ -60,17 +61,17 @@ def test_MeasurementsCollection_get(client):
     RESOURCE_URL_VALID = "/api/users/1/measurements/"
     RESOURCE_URL_INVALID = "/api/users/12/measurements/"
 
-    #Get from existing user
+    # Get from existing user
     resp = client.get(RESOURCE_URL_VALID)
     assert resp.status_code == 200
     cont = json.loads(resp.data)
     excepted = get_dummy_data_by_userid(1)
     cont = cont["measurements"]
     for item in cont:
-        del item["id"] #dummy data has no id as it is just the primary key in db
+        del item["id"]  # dummy data has no id as it is just the primary key in db
         assert item in excepted
 
-    #Get from nonexisting user
+    # Get from nonexisting user
     resp = client.get(RESOURCE_URL_INVALID)
     assert resp.status_code == 404
 
@@ -79,49 +80,46 @@ def test_MeasurementsCollection_post(client):
     RESOURCE_URL_VALID = "/api/users/1/measurements/"
     RESOURCE_URL_INVALID = "/api/users/12/measurements/"
     VALID_MEASUREMENT = {
-    "date": "2023-02-11T13:01:56",
-    "weight": 100,
-    "calories_in": 2500,
-    "calories_out": 2500,
-    "user_id": 1
+        "date": "2023-02-11T13:01:56",
+        "weight": 100,
+        "calories_in": 2500,
+        "calories_out": 2500,
+        "user_id": 1,
     }
-    INVALID_MEASUREMENT = {
-        "thisis": "invalid",
-        "asd": 1
-    }
+    INVALID_MEASUREMENT = {"thisis": "invalid", "asd": 1}
 
     INVALID_MEASUREMENT1 = {
-    "date": "2023-02-11T13:01:56",
-    "weight": "123",
-    "calories_in": 2500,
-    "calories_out": 2500,
-    "user_id": 1
+        "date": "2023-02-11T13:01:56",
+        "weight": "123",
+        "calories_in": 2500,
+        "calories_out": 2500,
+        "user_id": 1,
     }
     INVALID_MEASUREMENT2 = {
-    "date": "2023-02-11T13:01:56",
-    "weight": 100,
-    "calories_in": "123",
-    "calories_out": 2500,
-    "user_id": 1
+        "date": "2023-02-11T13:01:56",
+        "weight": 100,
+        "calories_in": "123",
+        "calories_out": 2500,
+        "user_id": 1,
     }
     INVALID_MEASUREMENT3 = {
-    "date": "2023-02-11T13:01:56",
-    "weight": 100,
-    "calories_in": 2500,
-    "calories_out": "123",
-    "user_id": 1
+        "date": "2023-02-11T13:01:56",
+        "weight": 100,
+        "calories_in": 2500,
+        "calories_out": "123",
+        "user_id": 1,
     }
 
-    #Valid exercise to valid user (also check that can be get)
+    # Valid exercise to valid user (also check that can be get)
     resp = client.post(RESOURCE_URL_VALID, json=VALID_MEASUREMENT)
     assert resp.status_code == 201
     resp = client.get(RESOURCE_URL_VALID)
     assert resp.status_code == 200
     res = json.loads(resp.data).get("measurements")[0]
-    del res["id"] #id is just the primary key
+    del res["id"]  # id is just the primary key
     assert res == VALID_MEASUREMENT
-    
-    #Invalid measurement to valid user
+
+    # Invalid measurement to valid user
     resp = client.post(RESOURCE_URL_VALID, json=INVALID_MEASUREMENT)
     assert resp.status_code == 400
 
@@ -134,11 +132,11 @@ def test_MeasurementsCollection_post(client):
     resp = client.post(RESOURCE_URL_VALID, json=INVALID_MEASUREMENT3)
     assert resp.status_code == 400
 
-    #Valid exercise to invalid user
+    # Valid exercise to invalid user
     resp = client.post(RESOURCE_URL_INVALID, json=INVALID_MEASUREMENT)
     assert resp.status_code == 404
-    
-    #Invalid exercise to invalid user
+
+    # Invalid exercise to invalid user
     resp = client.post(RESOURCE_URL_INVALID, json=INVALID_MEASUREMENT)
     assert resp.status_code == 404
 
@@ -148,7 +146,7 @@ def test_MeasurementsItem_get(client):
     RESOURCE_URL_VALID_WRONG_USER = "/api/users/1/measurements/5/"
     RESOURCE_URL_VALID_INVALID_USER = "/api/users/11/measurements/0/"
 
-    #Get measurment record corresponding to user
+    # Get measurment record corresponding to user
     resp = client.get(RESOURCE_URL_VALID)
     assert resp.status_code == 200
     res = json.loads(resp.data)
@@ -156,28 +154,28 @@ def test_MeasurementsItem_get(client):
     excepted = get_dummy_data_by_userid(3)
     assert res == excepted[0]
 
-    #Get existing exercise but corresponding to wrong user
+    # Get existing exercise but corresponding to wrong user
     resp = client.get(RESOURCE_URL_VALID_WRONG_USER)
     assert resp.status_code == 400
 
-    #Get existing exerciose but corresponding to non-existing user
+    # Get existing exerciose but corresponding to non-existing user
     resp = client.get(RESOURCE_URL_VALID_INVALID_USER)
     assert resp.status_code == 404
-    
+
 
 def test_MeasurementsItem_put(client):
     RESOURCE_URL_VALID = "/api/users/3/measurements/1/"
     RESOURCE_URL_VALID_WRONG_USER = "/api/users/1/measurements/1/"
     RESOURCE_URL_INVALID = "/api/users/1/measurements/100/"
     UPDATED_MEASUREMENT = {
-        "date": datetime.isoformat(datetime.now()), 
-        "weight": 80, 
+        "date": datetime.isoformat(datetime.now()),
+        "weight": 80,
         "calories_in": 2000,
         "calories_out": 2500,
-        "user_id": 3
+        "user_id": 3,
     }
 
-    #Update record
+    # Update record
     resp = client.put(RESOURCE_URL_VALID, json=UPDATED_MEASUREMENT)
     assert resp.status_code == 204
     resp = client.get(RESOURCE_URL_VALID)
@@ -187,12 +185,11 @@ def test_MeasurementsItem_put(client):
     del res["id"]
     assert res == UPDATED_MEASUREMENT
 
-    
-    #Update record with user_id mismatch
+    # Update record with user_id mismatch
     resp = client.put(RESOURCE_URL_VALID_WRONG_USER, json=UPDATED_MEASUREMENT)
     assert resp.status_code == 400
-    
-    #Update nonexisting record
+
+    # Update nonexisting record
     resp = client.put(RESOURCE_URL_INVALID, json=UPDATED_MEASUREMENT)
     assert resp.status_code == 404
 
@@ -202,16 +199,16 @@ def test_MeasurementsItem_delete(client):
     RESOURCE_URL_VALID_WRONG_USER = "/api/users/2/measurements/1/"
     RESOURCE_URL_INVALID = "/api/users/1/measurements/100/"
 
-    #Delete existing record (and verify deletion)
+    # Delete existing record (and verify deletion)
     resp = client.delete(RESOURCE_URL_VALID)
     assert resp.status_code == 204
     resp = client.get(RESOURCE_URL_VALID)
     assert resp.status_code == 404
 
-    #Try deleting from another user (should not be found)
+    # Try deleting from another user (should not be found)
     resp = client.delete(RESOURCE_URL_VALID_WRONG_USER)
     assert resp.status_code == 404
 
-    #Try deleting nonexisting record
+    # Try deleting nonexisting record
     resp = client.delete(RESOURCE_URL_INVALID)
     assert resp.status_code == 404
