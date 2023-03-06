6e096d1257da028e9497dc131dbd23a5b418c288
Lassi Tölli <lassi41@gmail.com>
2023-03-06 13:06:19 +0200

    Small changes on batch file

--- test/run_pytest.bat
+++ test/run_pytest.bat
@@ -1,12 +1,20 @@
 @ECHO OFF
-ECHO Starting pytest
-python3 -m pytest -vv --cov-report term-missing --cov=app
+set python_cmd=python3
+ECHO Select python command to use:
+ECHO 1. python
+ECHO 2. python3
+set /p input= Enter 1 or 2: 
+if %input% == 1 (
+    set python_cmd=python
+)
+ECHO Executing command: %python_cmd% -m pytest -vv --cov-report term-missing --cov=app
+%python_cmd% -m pytest -vv --cov-report term-missing --cov=app
 ECHO.
 ECHO ###################################################################################
 ECHO.
 ECHO Testing done. Press any key to delete temp files
 PAUSE
-python delete_test_files.py
+%python_cmd% delete_test_files.py
 ECHO.
 ECHO Temp files deleted
 ECHO.
